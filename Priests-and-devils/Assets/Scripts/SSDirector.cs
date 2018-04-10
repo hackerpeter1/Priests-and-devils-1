ce3f51134d6fc181b4caa7d8d146174587017c54
延泽 许 <peter13994@126.com>
2018-04-10 20:37:53 +0800

    commit

---
+++ Priests-and-devils/Assets/Scripts/SSDirector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MySecondGame
+{
+    public class SSDirector : System.Object
+    {
+        private static SSDirector _instance;
+        public ISceneController currentSceneController { get; set; }
+
+        public static SSDirector getInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new SSDirector();
+            }
+            return _instance;
+        }
+    }
+
+    public interface ISceneController
+    {
+        void loadResources();
+    }
+
+    public interface IUserController
+    {
+        void moveBoat();
+        void characterIsClicked(MyCharacterController characterCtrl);
+        void restart();
+    }
+}
+
